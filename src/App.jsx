874032706cf37bb6afd3330deda3fb583eac700d
Alex Messias <alexmessias_18@yahoo.com.br>
2026-05-24 15:40:42 -0300

    style: remove Analytics component from App for cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import "./styles/App.css"
 
-import { Analytics } from "@vercel/analytics/react"
 import { useAnalytics } from "./hooks/useAnalytics"
 
 import { useActiveSection } from "./hooks/useActiveSection"
@@ -54,8 +53,6 @@ export default function App() {
 
                 <ScrollToTop />
             </div>
-
-            <Analytics debug={false} />
         </>
     )
 }
